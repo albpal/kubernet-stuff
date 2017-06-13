53820670e95ea312eb7b5f39649152a67f863c9d
Albert Palau <anorkbcn@gmail.com>
2017-06-13 08:29:45 +0200

    Update close_port.sh

--- close_port.sh
+++ close_port.sh
@@ -1,26 +1,28 @@
 #!/bin/bash
 
-if [[ "$#" -ne 5 ]];then
+if [[ "$#" -ne 6 ]];then
 	echo "Illegal number of parameters"
-	echo "Usage: script.sh <PORT> <POD_INTERNAL_IP> <PUBLIC_NODE_INTERNAL_IP> <PUBLIC_INTERFACE> <PRIVATE_INTERFACE>"
+	echo "Usage: script.sh <PUBLIC_PORT> <CONTAINER_PORT> <POD_INTERNAL_IP> <PUBLIC_NODE_INTERNAL_IP> <PUBLIC_INTERFACE> <PRIVATE_INTERFACE>"
 	exit 1
 fi
 
-PORT=$1
-POD_INTERNAL_IP=$2
-PUBLIC_NODE_INTERNAL_IP=$3
-PUBLIC_INTERFACE=$4
-PRIVATE_INTERFACE=$5
+PUBLIC_PORT=$1
+CONTAINER_PORT=$2
+POD_INTERNAL_IP=$3
+PUBLIC_NODE_INTERNAL_IP=$4
+PUBLIC_INTERFACE=$5
+PRIVATE_INTERFACE=$6
 
-echo "sudo iptables -D FORWARD -i $PUBLIC_INTERFACE -o $PRIVATE_INTERFACE -p tcp --syn --dport $PORT -m conntrack --ctstate NEW -j ACCEPT"
-sudo iptables -D FORWARD -i $PUBLIC_INTERFACE -o $PRIVATE_INTERFACE -p tcp --syn --dport $PORT -m conntrack --ctstate NEW -j ACCEPT
+echo "sudo iptables -D FORWARD -i $PUBLIC_INTERFACE -o $PRIVATE_INTERFACE -p tcp --syn --dport $PUBLIC_PORT -m conntrack --ctstate NEW -j ACCEPT"
+sudo iptables -D FORWARD -i $PUBLIC_INTERFACE -o $PRIVATE_INTERFACE -p tcp --syn --dport $PUBLIC_PORT -m conntrack --ctstate NEW -j ACCEPT
 
 echo "sudo iptables -D FORWARD -i $PUBLIC_INTERFACE -o $PRIVATE_INTERFACE -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT"
 sudo iptables -D FORWARD -i $PUBLIC_INTERFACE -o $PRIVATE_INTERFACE -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
 echo "sudo iptables -D FORWARD -i $PRIVATE_INTERFACE -o $PUBLIC_INTERFACE -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT"
 sudo iptables -D FORWARD -i $PRIVATE_INTERFACE -o $PUBLIC_INTERFACE -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
 
-echo "sudo iptables -t nat -D PREROUTING -i $PUBLIC_INTERFACE -p tcp --dport $PORT -j DNAT --to-destination $POD_INTERNAL_IP"	
-sudo iptables -t nat -D PREROUTING -i $PUBLIC_INTERFACE -p tcp --dport $PORT -j DNAT --to-destination $POD_INTERNAL_IP
-echo "sudo iptables -t nat -D POSTROUTING -o $PRIVATE_INTERFACE -p tcp --dport $PORT -d $POD_INTERNAL_IP -j SNAT --to-source $PUBLIC_NODE_INTERNAL_IP"
-sudo iptables -t nat -D POSTROUTING -o $PRIVATE_INTERFACE -p tcp --dport $PORT -d $POD_INTERNAL_IP -j SNAT --to-source $PUBLIC_NODE_INTERNAL_IP
+echo "sudo iptables -t nat -D PREROUTING -i $PUBLIC_INTERFACE -p tcp --dport $PUBLIC_PORT -j DNAT --to-destination $POD_INTERNAL_IP:$CONTAINER_PORT"	
+sudo iptables -t nat -D PREROUTING -i $PUBLIC_INTERFACE -p tcp --dport $PUBLIC_PORT -j DNAT --to-destination $POD_INTERNAL_IP:$CONTAINER_PORT
+echo "sudo iptables -t nat -D POSTROUTING -o $PRIVATE_INTERFACE -p tcp --dport $PUBLIC_PORT -d $POD_INTERNAL_IP -j SNAT --to-source $PUBLIC_NODE_INTERNAL_IP:$PUBLIC_PORT"
+sudo iptables -t nat -D POSTROUTING -o $PRIVATE_INTERFACE -p tcp --dport $PUBLIC_PORT -d $POD_INTERNAL_IP -j SNAT --to-source $PUBLIC_NODE_INTERNAL_IP:$PUBLIC_PORT
+
