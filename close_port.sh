6b8f4b59d5b2828dff5b085c9e0f2936f5c7ba70
Albert Palau <anorkbcn@gmail.com>
2017-06-16 17:14:56 +0200

    Add comment to every iptable rule

--- close_port.sh
+++ close_port.sh
@@ -13,16 +13,15 @@ PUBLIC_NODE_INTERNAL_IP=$4
 PUBLIC_INTERFACE=$5
 PRIVATE_INTERFACE=$6
 
-echo "sudo iptables -D FORWARD -i $PUBLIC_INTERFACE -o $PRIVATE_INTERFACE -p tcp --syn --dport $PUBLIC_PORT -m conntrack --ctstate NEW -j ACCEPT"
-sudo iptables -D FORWARD -i $PUBLIC_INTERFACE -o $PRIVATE_INTERFACE -p tcp --syn --dport $PUBLIC_PORT -m conntrack --ctstate NEW -j ACCEPT
+echo "sudo iptables -D FORWARD -i $PUBLIC_INTERFACE -o $PRIVATE_INTERFACE -p tcp --syn --dport $PUBLIC_PORT -m conntrack --ctstate NEW -j ACCEPT -m comment --comment \"Open port from $PUBLIC_PORT to $POD_INTERNAL_IP:$CONTAINER_PORT\""
+sudo iptables -D FORWARD -i $PUBLIC_INTERFACE -o $PRIVATE_INTERFACE -p tcp --syn --dport $PUBLIC_PORT -m conntrack --ctstate NEW -j ACCEPT -m comment --comment "Open port from $PUBLIC_PORT to $POD_INTERNAL_IP:$CONTAINER_PORT"
 
-echo "sudo iptables -D FORWARD -i $PUBLIC_INTERFACE -o $PRIVATE_INTERFACE -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT"
-sudo iptables -D FORWARD -i $PUBLIC_INTERFACE -o $PRIVATE_INTERFACE -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
-echo "sudo iptables -D FORWARD -i $PRIVATE_INTERFACE -o $PUBLIC_INTERFACE -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT"
-sudo iptables -D FORWARD -i $PRIVATE_INTERFACE -o $PUBLIC_INTERFACE -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
+echo "sudo iptables -D FORWARD -i $PUBLIC_INTERFACE -o $PRIVATE_INTERFACE -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT -m comment --comment \"Open port from $PUBLIC_PORT to $POD_INTERNAL_IP:$CONTAINER_PORT\""
+sudo iptables -D FORWARD -i $PUBLIC_INTERFACE -o $PRIVATE_INTERFACE -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT -m comment --comment "Open port from $PUBLIC_PORT to $POD_INTERNAL_IP:$CONTAINER_PORT"
+echo "sudo iptables -D FORWARD -i $PRIVATE_INTERFACE -o $PUBLIC_INTERFACE -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT -m comment --comment \"Open port from $PUBLIC_PORT to $POD_INTERNAL_IP:$CONTAINER_PORT\""
+sudo iptables -D FORWARD -i $PRIVATE_INTERFACE -o $PUBLIC_INTERFACE -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT -m comment --comment "Open port from $PUBLIC_PORT to $POD_INTERNAL_IP:$CONTAINER_PORT"
 
-echo "sudo iptables -t nat -D PREROUTING -i $PUBLIC_INTERFACE -p tcp --dport $PUBLIC_PORT -j DNAT --to-destination $POD_INTERNAL_IP:$CONTAINER_PORT"	
-sudo iptables -t nat -D PREROUTING -i $PUBLIC_INTERFACE -p tcp --dport $PUBLIC_PORT -j DNAT --to-destination $POD_INTERNAL_IP:$CONTAINER_PORT
-echo "sudo iptables -t nat -D POSTROUTING -o $PRIVATE_INTERFACE -p tcp --dport $PUBLIC_PORT -d $POD_INTERNAL_IP -j SNAT --to-source $PUBLIC_NODE_INTERNAL_IP:$PUBLIC_PORT"
-sudo iptables -t nat -D POSTROUTING -o $PRIVATE_INTERFACE -p tcp --dport $PUBLIC_PORT -d $POD_INTERNAL_IP -j SNAT --to-source $PUBLIC_NODE_INTERNAL_IP:$PUBLIC_PORT
-
+echo "sudo iptables -t nat -D PREROUTING -i $PUBLIC_INTERFACE -p tcp --dport $PUBLIC_PORT -m comment --comment \"Open port from $PUBLIC_PORT to $POD_INTERNAL_IP:$CONTAINER_PORT\" -j DNAT --to-destination $POD_INTERNAL_IP:$CONTAINER_PORT"	
+sudo iptables -t nat -D PREROUTING -i $PUBLIC_INTERFACE -p tcp --dport $PUBLIC_PORT -m comment --comment "Open port from $PUBLIC_PORT to $POD_INTERNAL_IP:$CONTAINER_PORT" -j DNAT --to-destination "$POD_INTERNAL_IP:$CONTAINER_PORT"
+echo "sudo iptables -t nat -D POSTROUTING -o $PRIVATE_INTERFACE -p tcp --dport $PUBLIC_PORT -m comment --comment \"Open port from $PUBLIC_PORT to $POD_INTERNAL_IP:$CONTAINER_PORT\" -d $POD_INTERNAL_IP -j SNAT --to-source $PUBLIC_NODE_INTERNAL_IP:$PUBLIC_PORT"
+sudo iptables -t nat -D POSTROUTING -o $PRIVATE_INTERFACE -p tcp --dport $PUBLIC_PORT -m comment --comment "Open port from $PUBLIC_PORT to $POD_INTERNAL_IP:$CONTAINER_PORT" -d $POD_INTERNAL_IP -j SNAT --to-source "$PUBLIC_NODE_INTERNAL_IP:$PUBLIC_PORT"
